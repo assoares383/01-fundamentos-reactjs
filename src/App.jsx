eb60bc254bb474f84fd98971b1e8b7bb8691b717
= <alexandressoares383@gmail.com>
2024-01-03 20:51:50 -0300

    feat: add props

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
   return (
     <div>
       <h1>Hello World!!!</h1>
-      <Post />
+      <Post 
+        author="Alexandre Soares" 
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut leo libero, pharetra a laoreet eget, interdum sed metus." 
+      />
     </div>
   );
 }
