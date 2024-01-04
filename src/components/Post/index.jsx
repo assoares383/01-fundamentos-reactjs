d7a5a8f9a069c3d85a17bd66e2347c53a5dd49f8
= <alexandressoares383@gmail.com>
2024-01-04 17:58:36 -0300

    feat: add component post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -2,6 +2,33 @@ import styles from './Post.module.css'
 
 export function Post() {
     return (
-        <div className={styles.post}></div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img 
+                        className={styles.avatar}
+                        src="https://avatars.githubusercontent.com/u/15836394?v=4"
+                    />
+                </div>
+                <div className={styles.authorInfo}>
+                    <strong>Alexandre Soares</strong>
+                    <span>Web Developer</span>
+                </div>
+                <time dateTime='04 de Janeiro as 17:55h'>
+                    Publicado ha 1h
+                </time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>
+                    <a href="#">#nlw</a>
+                    <a href="#">#rocketseat</a>
+                </p>
+            </div>
+        </article>
     )
 }
